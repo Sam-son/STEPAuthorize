d2ffecb472c6258bcd9f193f8f35033e8c0e7c99
Samson Bonfante <Samson@bonfante.org>
2014-06-11 09:15:35 -0400

    Fixed handling of files with spaces in filename.

--- STEPAuth.bat
+++ STEPAuth.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-
+setlocal 
 ::Initialize all the variables we may use.
 set OPENSSL_CONF=.\openssl.cfg
 set "signsig="
@@ -11,12 +11,14 @@ set "signaturedec="
 set "data="
 set "pub="
 
-if "%1"=="VERIFY" GOTO verify
-if "%1"=="verify" GOTO verify
-if "%1"=="SIGN" GOTO sign
-if "%1"=="sign" GOTO sign
-if "%1"=="CSR" GOTO csr
-if "%1"=="csr" GOTO csr
+set command=%~1
+echo %command%
+if "%command%"=="VERIFY" GOTO verify
+if "%command%"=="verify" GOTO verify
+if "%command%"=="SIGN" GOTO sign
+if "%command%"=="sign" GOTO sign
+if "%command%"=="CSR" GOTO csr
+if "%command%"=="csr" GOTO csr
 GOTO usage
 
 :usage
@@ -24,37 +26,38 @@ echo %0 ^[CSR^, VERIFY^, SIGN^]
 GOTO end
 
 :verify
-if "%2"=="/?" GOTO helpverify
-if "%2"=="" GOTO helpverify
+set infile=%~2
+if "%infile%"=="/?" GOTO helpverify
+if "%infile%"=="" GOTO helpverify
 
-StepBreak %2
+StepBreak "%infile%"
 if ERRORLEVEL 1 goto error
 
-set "certificate=%2.cert"
-set "signature=%2.signature"
-set "signaturedec=%2.sig"
-set "data=%2.tmp"
+set "certificate=%infile%.cert"
+set "signature=%infile%.signature"
+set "signaturedec=%infile%.sig"
+set "data=%infile%.tmp"
 set "strip=%data%.stripped"
-set "pub=%2.pub"
-STEPStrip %data%
+set "pub=%infile%.pub"
+STEPStrip "%data%"
 if ERRORLEVEL 1 goto error
-openssl x509 -pubkey -noout -in %certificate% >%pub%
+openssl x509 -pubkey -noout -in "%certificate%" >"%pub%"
 IF ERRORLEVEL 1 GOTO error
 ::Print Subject
-openssl x509 -in %certificate% -subject -noout -nameopt multiline
+openssl x509 -in "%certificate%" -subject -noout -nameopt multiline
 if ERRORLEVEL 1 goto error
-openssl enc -d -base64 -in %signature% -out %signaturedec%
+openssl enc -d -base64 -in "%signature%" -out "%signaturedec%"
 if ERRORLEVEL 1 goto error
 echo.
 echo.
 echo Verifying certificate^.^.^.
-openssl verify -CAfile root-ca.crt %certificate%
+openssl verify -CAfile root-ca.crt "%certificate%"
 if ERRORLEVEL 1 goto end
 echo.
 echo.
 
 echo Verifying signature^.^.^.
-openssl dgst -sha256 -verify %pub% -signature %signaturedec% %strip%
+openssl dgst -sha256 -verify "%pub%" -signature "%signaturedec%" "%strip%"
 echo.
 GOTO cleanup
 
@@ -63,51 +66,54 @@ echo %0 VERIFY ^<data file^>
 GOTO end
 
 :sign
-IF "%2"=="" GOTO helpsign
-IF "%2"=="/?" GOTO helpsign
-IF "%3"=="" GOTO helpsign
-IF "%4"=="" GOTO helpsign
+set infile=%~2
+set privatekey=%~3
+set incert=%~4
+IF "%infile%"=="" GOTO helpsign
+IF "%infile%"=="/?" GOTO helpsign
+IF "%privatekey%"=="" GOTO helpsign
+IF "%incert%"=="" GOTO helpsign
 
-SET "signsig64=%2.signature"
-SET "outfile=signed_%2"
-STEPStrip %2
+SET "signsig64=%infile%.signature"
+SET "outfile=signed_%infile%"
+STEPStrip "%infile%"
 if ERRORLEVEL 1 goto error
 
-SET "strip=%2.stripped"
-SET "signsig=%2.sig"
-openssl dgst -sha256 -sign %3 -out %signsig% %strip%
+SET "strip=%infile%.stripped"
+SET "signsig=%infile%.sig"
+openssl dgst -sha256 -sign "%privatekey%" -out "%signsig%" "%strip%"
 if ERRORLEVEL 1 goto error
 
-openssl enc -base64 -in %signsig% |repl "\n" "\r\n" xm > %signsig64%
+openssl enc -base64 -in "%signsig%" |repl "\n" "\r\n" xm > "%signsig64%"
 if ERRORLEVEL 1 goto error
 
-copy %2 %outfile% >NUL
+copy "%infile%" "%outfile%" >NUL
 ::Print the Signature to outfile. Looks like:
 ::SIGNATURE;
 ::[bunch of lines of signature stuff]
 ::ENDSEC;
-echo.>> %outfile%
-echo SIGNATURE^;>> %outfile%
-type %signsig64%>> %outfile%
-echo ENDSEC^;>> %outfile%
-echo.>> %outfile%
+echo.>> "%outfile%"
+echo SIGNATURE^;>> "%outfile%"
+type "%signsig64%">> "%outfile%"
+echo ENDSEC^;>> "%outfile%"
+echo.>> "%outfile%"
 ::Print Public key. Gotten right from the certificate. 
 ::Similar to Signature, section begins with:
 ::PUBLIC KEY;
 ::Skip this if -old flag isn't set.
 IF NOT "%5"=="-old" GOTO printcert 
-echo PUBLIC KEY^;>> %outfile%
-openssl x509 -pubkey -noout -in %4 |repl "\n" "\r\n" xm >> %outfile%
-echo ENDSEC^;>>%outfile%
-echo.>> %outfile%
+echo PUBLIC KEY^;>> "%outfile%"
+openssl x509 -pubkey -noout -in "%incert%" |repl "\n" "\r\n" xm >> "%outfile%"
+echo ENDSEC^;>>"%outfile%"
+echo.>> "%outfile%"
 
 :printcert
 ::Print Certificate to file. Begins with:
 ::CERTIFICATE;
-echo CERTIFICATE^;>> %outfile%
-openssl x509 -outform pem -in %4 >> %outfile%
-echo.>> %outfile%
-echo ENDSEC^;>> %outfile%
+echo CERTIFICATE^;>> "%outfile%"
+openssl x509 -outform pem -in "%incert%" >> "%outfile%"
+echo.>> "%outfile%"
+echo ENDSEC^;>> "%outfile%"
 
 ::Done with everything. Delete temporary files.
 GOTO cleanup
@@ -117,8 +123,9 @@ echo %0 SIGN ^<file to sign^> ^<private key^> ^<certificate^>
 GOTO end
 
 :csr
-if "%2"=="" GOTO helpcsr
-openssl req -new -config cert.conf -out %2.csr -keyout %2-private.key
+set infile=%~2
+if "%infile%"=="" GOTO helpcsr
+openssl req -new -config cert.conf -out "%infile%.csr" -keyout "%infile%-private.key"
 if ERRORLEVEL 1 goto error
 GOTO end
 
@@ -132,14 +139,14 @@ GOTO cleanup
 
 :cleanup
 ::Check if any temporary files exist and remove them.
-IF NOT "%signsig%"=="" del %signsig%
-IF NOT "%signsig64%"=="" del %signsig64%
-IF NOT "%strip%"=="" del %strip%
-IF NOT "%certificate%"=="" del %certificate% 
-IF NOT "%signature%"=="" del %signature% 
-IF NOT "%signaturedec%"=="" del %signaturedec%
-IF NOT "%data%"=="" del %data%
-IF NOT "%pub%"=="" del %pub%
+IF NOT "%signsig%"=="" del "%signsig%"
+IF NOT "%signsig64%"=="" del "%signsig64%"
+IF NOT "%strip%"=="" del "%strip%"
+IF NOT "%certificate%"=="" del "%certificate%"
+IF NOT "%signature%"=="" del "%signature%"
+IF NOT "%signaturedec%"=="" del "%signaturedec%"
+IF NOT "%data%"=="" del "%data%"
+IF NOT "%pub%"=="" del "%pub%"
 GOTO end
 
 :end
